0b31d441cd3d839153e1513e970b0635f0801426
quantium-rock <alexcollsoutumuro@gmail.com>
2022-05-21 19:17:44 +0200

    restructure files

---
+++ cryptodash/src/App/index.js
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    );
+  }
+}
+
+export default App;

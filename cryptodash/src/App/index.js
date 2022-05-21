7cd8f9253ef85cb9e49f756c76848c601024364d
quantium-rock <alexcollsoutumuro@gmail.com>
2022-05-21 19:30:34 +0200

    cryptodash

--- cryptodash/src/App/index.js
+++ cryptodash/src/App/index.js
@@ -1,11 +1,24 @@
-import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 import './App.css';
+import AppLayout from './AppLayout';
+import AppBar from './AppBar';
+import {AppProvider} from './AppProvider';
+import Settings from '../Settings';
+import Dashboard from '../Dashboard';
+import Content from '../Shared/Content';
 
 class App extends Component {
   render() {
     return (
-      <div>Hello world</div>
+      <AppLayout>
+        <AppProvider>
+          <AppBar/>
+          <Content>
+            <Settings />
+            <Dashboard />
+          </Content>
+        </AppProvider>
+      </AppLayout>
     );
   }
 }
